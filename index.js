2d4c9c75cff78d15675e1c9a8b4cb431cfe17aa5
kind-prototype-8380 <dk2800@nyu.edu>
2020-08-25 21:33:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -121,10 +121,11 @@ function endGame() {
 }
 
 function moveDodger(e) {
-  e.preventDefault();
-  e.stopPropagation();
+  if (e.which===LEFT_ARROW || e.which===RIGHT_ARROW) {
+ e.preventDefault();
+ e.stopPropagation()
+  }
   if (e.which===LEFT_ARROW) {moveDodgerLeft()}
-  
   if (e.which===RIGHT_ARROW) {moveDodgerRight()}
 }
   
